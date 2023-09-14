42ff4ea0e8f211148d0c1c08d0f7d87515fc6cef
Johan Nicklasson <johan.nicklasson@easyfy.se>
2023-09-14 09:50:07 +0200

    changes accourding to PR in ASPNET repo for net7, Prevent NRE when body is not set #46613

--- src/OutputCacheEntry.cs
+++ src/OutputCacheEntry.cs
@@ -7,28 +7,28 @@ namespace Microsoft.AspNetCore.OutputCaching;
 
 internal sealed class OutputCacheEntry
 {
-    /// <summary>
-    /// Gets the created date and time of the cache entry.
-    /// </summary>
-    public DateTimeOffset Created { get; set; }
+  /// <summary>
+  /// Gets the created date and time of the cache entry.
+  /// </summary>
+  public DateTimeOffset Created { get; set; }
 
-    /// <summary>
-    /// Gets the status code of the cache entry.
-    /// </summary>
-    public int StatusCode { get; set; }
+  /// <summary>
+  /// Gets the status code of the cache entry.
+  /// </summary>
+  public int StatusCode { get; set; }
 
-    /// <summary>
-    /// Gets the headers of the cache entry.
-    /// </summary>
-    public HeaderDictionary Headers { get; set; } = default!;
+  /// <summary>
+  /// Gets the headers of the cache entry.
+  /// </summary>
+  public HeaderDictionary? Headers { get; set; }
 
-    /// <summary>
-    /// Gets the body of the cache entry.
-    /// </summary>
-    public CachedResponseBody Body { get; set; } = default!;
+  /// <summary>
+  /// Gets the body of the cache entry.
+  /// </summary>
+  public CachedResponseBody? Body { get; set; }
 
-    /// <summary>
-    /// Gets the tags of the cache entry.
-    /// </summary>
-    public string[] Tags { get; set; } = Array.Empty<string>();
+  /// <summary>
+  /// Gets the tags of the cache entry.
+  /// </summary>
+  public string[] Tags { get; set; } = Array.Empty<string>();
 }
